9b106505547bd16926abc040505684dd097ef5a3
Louise Vida <vida@upperyeah.com>
2026-03-11 15:11:15 +0100

    Update main.py

--- main.py
+++ main.py
@@ -344,7 +344,7 @@ def contour_to_svg(
     pad: int = 30,
 ) -> str:
     if crop_to_subject:
-    contour, width, height = crop_contour_to_subject(contour, width, height, pad=pad)
+        contour, width, height = crop_contour_to_subject(contour, width, height, pad=pad)
 
 contour = anchor_contour_to_bottom(contour, height)
 contour = open_contour_at_bottom(contour, height=height, bleed=0)
